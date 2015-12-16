4a99be3c736162eb81587f35aa3f82ab46d58dc5
Palle Cogburn <palle@4thex.com>
2015-12-16 13:44:44 +0000

    Added more methods

--- app/index.js
+++ app/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
-var event = require('./modules/event')(app);
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+var persistence = require("./modules/persistence")(app);
+var event = require('./modules/event')(app, persistence);
 var swagger = require('./modules/swagger')(app);
 
 app.use('/api/docs', express.static('client/api/swagger/swagger-ui/dist'));
--- app/modules/event/index.js
+++ app/modules/event/index.js
@@ -1,5 +1,43 @@
-module.exports = function (app) {
-    
+module.exports = function (app, persistence) {
+  app.route("/api/events/:id")
+    .get(function(req, res) {
+      persistence.retrieve(req.params.id)
+        .then(function(event) {
+          res.json(event);
+        })
+        .catch(function(error) {
+          res.status(500).send(error);
+        });
+    })
+    .delete(function(req, res) {
+      persistence.delete(req.params.id)
+        .then(function(event) {
+          res.json(event);
+        })
+        .catch(function(error) {
+          res.status(500).send(error);
+        });
+    });
+  app.route("/api/events")
+    .get(function(req, res) {
+      persistence.find()
+        .then(function(events) {
+          res.status(200).json(events);
+        })
+        .catch(function(error) {
+          res.status(500).send(error);
+        });
+    })
+    .put(function(req, res) {
+      var event = req.body;
+      persistence.save(event)
+        .then(function(event) {
+          res.status(201).json(event).send("Created");
+        })
+        .catch(function(error) {
+          res.status(500).send(error);
+        });
+    });
     return {
         
     };
